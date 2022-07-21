22f1bb412e764909642c5856238f6f7129320591
Piotr Z. Jelonek <piotr.z.jelonek@gmail.com>
2022-07-21 19:56:56 +0100

    A comment how to run Firefox in headless mode

--- selenium_test.py
+++ selenium_test.py
@@ -60,5 +60,17 @@ link = a_tag.get_attribute('href')
 print("\nlink:")
 print(link)
 
+# # running a driver in headless mode
+
+# from selenium import webdriver
+# from selenium.webdriver.firefox.options import Options
+
+# options = Options()
+# options.headless = True
+# driver = webdriver.Firefox(options=options, executable_path=r'C:\Utility\BrowserDrivers\geckodriver.exe')
+# driver.get("http://google.com/")
+# print ("Headless Firefox Initialized")
+# driver.quit()
+
 # Questions:
 # 1) How to run locally Colab files
