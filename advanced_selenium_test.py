eb2f24d8537c1b3ecb3724e970e862859d27c9a9
Piotr Z. Jelonek <piotr.z.jelonek@gmail.com>
2022-08-03 21:12:52 +0100

    Load_and_accept_cookies() - more advanced version

--- advanced_selenium_test.py
+++ advanced_selenium_test.py
@@ -1,8 +1,21 @@
+# import libraries
+import time
+
+# import classes
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
+# initialize
 driver = webdriver.Firefox()
+
+# If you can explain it in words - selenium probably can do it. Just google it -
+# or check the documentation
+
+# Selenium 4.1: https://selenium-python.readthedocs.io/locating-elements.html 
 
 # go to python website
 driver.get("http://www.python.org")
@@ -19,3 +32,46 @@ search_bar.send_keys("method")
 
 # press enter
 search_bar.send_keys(Keys.RETURN)
+
+# Many websites are dynamic, their whole content is not available right after connecting to them. 
+# In this case, Selenium will try to find elements before the whole page is loaded, 
+# and the scraper will fail if the element is not ready.
+
+def load_and_accept_cookies() -> webdriver.Firefox:
+    '''
+    Open Zoopla and accept the cookies
+    
+    Returns
+    -------
+    driver: webdriver.Firefox
+        This driver is already in the Zoopla webpage
+    '''
+    
+    driver = webdriver.Firefox() 
+    URL = "https://www.zoopla.co.uk/new-homes/property/london/?q=London&results_sort=newest_listings&search_source=new-homes&page_size=25&pn=1&view_type=list"
+    driver.get(URL)
+    maximum_delay = 10
+
+    try:
+        WebDriverWait(driver, maximum_delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="gdpr-consent-notice"]')))
+        driver.switch_to.frame('gdpr-consent-notice')
+        print("Frame Ready!")
+        
+        try:
+            accept_cookies_button = WebDriverWait(driver, maximum_delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="save"]')))
+            accept_cookies_button.click()
+            print("Accept Cookies Button Ready!")
+
+        except TimeoutException:
+
+            print("Loading a botton took too much time!")
+        
+        time.sleep(1)
+
+    except TimeoutException:
+        print("Loading a frame took too much time!")
+
+    return driver 
+
+# the code
+driver = load_and_accept_cookies()
