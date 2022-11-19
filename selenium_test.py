01204dfd455226b0a0f5a68ecafb00a9fb8c6ffa
Piotr Z. Jelonek <piotr.z.jelonek@gmail.com>
2022-11-19 21:05:08 +0000

    Close pop-up. Parse scrapped tex and convert to numeric.

--- selenium_test.py
+++ selenium_test.py
@@ -1,13 +1,14 @@
 # selenium can 'drive' a browser
 
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait 
-# from selenium.webdriver.support import expected_conditions as EC 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 # Selenium 4.1: https://selenium-python.readthedocs.io/locating-elements.html 
-
 
 def load_and_accept_cookies() -> webdriver.Firefox:
     '''
@@ -19,6 +20,7 @@ def load_and_accept_cookies() -> webdriver.Firefox:
         This driver is already in the Zoopla webpage
     '''
 
+    # driver = webdriver.Chrome(ChromeDriverManager().install())
     driver = webdriver.Firefox()
     URL =    "https://www.zoopla.co.uk/new-homes/property/london/?q=London&results_sort=newest_listings&search_source=new-homes&page_size=25&pn=1&view_type=list"
     driver.get(URL)
@@ -49,21 +51,21 @@ def load_and_accept_cookies() -> webdriver.Firefox:
     return driver 
 
 
-def get_link_list(driver: webdriver.Chrome) -> list:
+def get_link_list(driver: webdriver.Firefox) -> list:
     '''
     Returns a list with all the links in the current page
     Parameters
     ----------
-    driver: webdriver.Chrome
+    driver: webdriver.Firefox
         The driver that contains information about the current page
     
     Returns
     -------
     link_list: list
-        A list with all the links in the page
+        A list with all the links in the page css-1itfubx ebjry2p0
     '''
 
-    prop_container = driver.find_element(by=By.XPATH, value='//div[@class="css-1itfubx ebjry2p0"]')
+    prop_container = driver.find_element(by=By.XPATH, value='//div[@data-testid="regular-listings"]')
     prop_list = prop_container.find_elements(by=By.XPATH, value='./div')
     link_list = []
 
@@ -80,40 +82,31 @@ driver = load_and_accept_cookies()
 nof_pages = 1
 s_next = '//div[@data-testid="pagination"]/ul//li/a[@aria-label="Page 1"]'
 
-for i in range(nof_pages): # The first 5 pages only
+for page_number in range(nof_pages): # The first 5 pages only
+
+    print(f"i: {page_number}")
 
     time.sleep(2)
+
     big_list.extend(get_link_list(driver)) # Call the function we just created and extend the big list with the returned list
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") 
-    # next_page_button = driver.find_element(by=By.XPATH, value='//ul[@class="eybokor8 css-1gny8z8-PaginationContainer-Pagination eaoxhri0"]') # <- this works ;)
+
+    button_container = driver.find_element(by=By.XPATH, value='//div[@data-testid="pagination"]')
+    a_tags = button_container.find_elements(by=By.TAG_NAME, value='a')
+    for links in a_tags:
+        if links.text == "Next >":
+            print("\nFound it")
+            next_page_button = links.get_attribute('href')
+    driver.get(next_page_button)
     
-    s = s_next.replace(str(int(i+1)), str(int(i+2)))
-    print(s)
-    next_page_button = driver.find_element(by=By.XPATH, value=s)
-    next_page_button.click()
+    time.sleep(2)
 
-    sing_up_for_alerts = driver.find_element(by=By.XPATH, value='/html/body/div[9]')
-    # sing_up_for_alerts = driver.find_element(by=By.XPATH, value='//div[@class="css-7ohbj1-ModalBg e13xjwxo16"]')
+    if page_number==0:
+        main_container = driver.find_element(by=By.XPATH, value='//div[@data-testid="modal-bg"]/div/div/button')
+        sing_up_for_alerts = main_container.find_element(By.XPATH, "./*[name()='svg']")
+        ActionChains(driver).move_to_element(sing_up_for_alerts).click().perform()
 
-    # sing_up_for_alerts = driver.find_element(by=By.XPATH, value='//div/div[@tabindex="-1"]')
-
-
-
-
-
-    # next_page_button = driver.find_element(by=By.XPATH, value='//ul[@class="eybokor8 css-1gny8z8-PaginationContainer-Pagination eaoxhri0"]')
-    # next_page_button = driver.find_element(by=By.XPATH, value='/html/body/div[3]/div/main/div/div[4]/div[2]/section/div[2]/div[3]/ul/li[8]/a')
-
-    # value_string = './/li[@href="https://www.zoopla.co.uk/new-homes/property/london/?q=London&results_sort=newest_listings&search_source=new-homes&page_size=25&pn=2"]'
-    # next_page_button  = driver.find_element(by=By.XPATH, value=value_string )
-    #next_page_button.click()
-
-    ## TODO: Click the next button. Don't forget to use sleeps, so the website doesn't suspect
-    pass # This pass should be removed once the code is complete
-
-
-# print(big_list)
-
+# print the list
 nof_links = len(big_list)
 print("")
 print(f"Total number of links: {nof_links}")
@@ -127,89 +120,83 @@ for i in range(nof_links):
 #     ## TODO: Visit all the links, and extract the data. Don't forget to use sleeps, so the website doesn't suspect
 #     pass # This pass should be removed once the code is complete
 
-# driver.quit() # Close the browser when you finish
+# driver.quit() # Close the browser when you finish        
 
+# create placeholder for scraped data
+dict_properties = {'Price': [], 'Address': [], 'Bedrooms': [], 'Bathrooms': [], 'Receptions': [], 'Description': []}
 
+nof_properties = 10
+for i in range(nof_properties):
 
+    print(f"i: {i}")
+    driver.get(big_list[i]) 
 
-
-
-
-
-
-
-
-
-# # accept cookies
-# driver = load_and_accept_cookies()  
-
-# time.sleep(2)
-
-# # try: 
-
-# prop_container = driver.find_element(by=By.XPATH, value='//div[@class="css-1itfubx e3tdh350"]') # XPath corresponding to the Container
-
-# prop_list = prop_container.find_elements(by=By.XPATH, value='./div')
-# link_list = []
-
-# for house_property in prop_list:
-#     a_tag = house_property.find_element(by=By.TAG_NAME, value='a')
-#     link = a_tag.get_attribute('href')
-#     link_list.append(link)
+    # get price
+    price = driver.find_element(by=By.XPATH, value='//p[@data-testid="price"]').text
     
-# print(f'There are {len(link_list)} properties in this page')
+    numeric_price = int(price[1:].replace(',',''))
+    dict_properties['Price'].append(numeric_price )
 
-# for link in link_list:
-#     print(f"  {link}") 
+    # get address
+    address = driver.find_element(by=By.XPATH, value='//address[@data-testid="address-label"]').text
+    dict_properties['Address'].append(address)
+
+    # get all the rooms
+    rooms = driver.find_element(by=By.XPATH, value='//div[@class="c-PJLV c-PJLV-iiNveLf-css"]').text
+    
+    # parse and read numbers
+    rooms_list = rooms.split('\n')
+
+    bed_count = 0
+    bath_count = 0
+    lounge_count = 0
+    for room in rooms_list:
+        room_parsed = room.split(' ')
+        count = int(room_parsed[0])
+        tag = room_parsed[1].lower()
+        if bed_count == 0:
+            if tag in ['bed','beds','bedroom','bedroom']:
+                bed_count = count
+        if bath_count == 0:
+            if tag in ['bath','baths','bathroom','bahrooms']:
+                bath_count = count
+        if lounge_count == 0:
+            if tag in ['lounge','lounges','reception','receptions']:
+                lounge_count = count
+
+        #print(room)
+
+    dict_properties['Bedrooms'].append(bed_count)
+    dict_properties['Bathrooms'].append(bath_count)
+    dict_properties['Receptions'].append(lounge_count)
+
+    print("") 
+    print(numeric_price)
+    print(bed_count, bath_count, lounge_count)
+    print("")
+
+    # print(rooms_list,'\n')
+
+    # bedrooms = int(rooms_list[0])
+    # bathrooms = int(rooms_list[2])
+    # receptions= int(rooms_list[4])
+
+    # print(bedrooms, bathrooms, receptions)
+    # print("")
 
 
+    # dict_properties['Bedrooms'].append(bedrooms)
 
-# # except:
+    # print(bedrooms)
 
-# #     print('duupa')
+    # get description
+    div_tag = driver.find_element(by=By.XPATH, value='//div[@data-testid="truncated_text_container"]')
+    span_tag = div_tag.find_element(by=By.XPATH, value='.//span')
+    description = span_tag.text
+    dict_properties['Description'] = description
 
-
-# # Note: below we have to change @id to a value that certainly is on THIS page 
-
-# # find a listing on a given page, scrap text
-# house_property = driver.find_element(by=By.XPATH, value='//*[@id="listing_62731443"]')
-# s = house_property.text
-
-# # output
-# print("\nhouse_property.text: ")
-# print(s)
-
-# # find a link on a given page, get 'href'
-# a_tag = house_property.find_element(by=By.TAG_NAME, value='a')
-# link = a_tag.get_attribute('href')
-
-# print("\nlink:")
-# print(link)
-
-# time.sleep(2)
-
-# driver.get(link)
-
-# # create placeholder for scraped data
-# dict_properties = {'Price': [], 'Address': [], 'Bedrooms': [], 'Description': []}
-
-# # get price
-# price = driver.find_element(by=By.XPATH, value='//p[@data-testid="price"]').text
-# dict_properties['Price'].append(price)
-
-# # get address
-# address = driver.find_element(by=By.XPATH, value='//address[@data-testid="address-label"]').text
-# dict_properties['Address'].append(address)
-
-# # get bedrooms
-# bedrooms = driver.find_element(by=By.XPATH, value='//div[@class="c-PJLV c-PJLV-iiNveLf-css"]').text
-# dict_properties['Bedrooms'].append(bedrooms)
-
-# # get description
-# div_tag = driver.find_element(by=By.XPATH, value='//div[@data-testid="truncated_text_container"]')
-# span_tag = div_tag.find_element(by=By.XPATH, value='.//span')
-# description = span_tag.text
-# dict_properties['Description'] = description
+    # sleep
+    time.sleep(1)
 
 # # output
 # for key in dict_properties.keys():
@@ -223,45 +210,27 @@ for i in range(nof_links):
 #         print(f"  {dict_properties[key]}")
 
 
-# # close the browser
+# # running a driver in headless mode
+
+# from selenium import webdriver
+# from selenium.webdriver.firefox.options import Options
+
+# options = Options()
+# options.headless = True
+# driver = webdriver.Firefox(options=options, executable_path=r'C:\Utility\BrowserDrivers\geckodriver.exe')
+# driver.get("http://google.com/")
+# print ("Headless Firefox Initialized")
 # driver.quit()
 
-# # dict_properties['Price'].append(price)
-# # address = driver.find_element(by=By.XPATH, value='//address[@data-testid="address-label"]').text
-# # dict_properties['Address'].append(address)
-# # bedrooms = driver.find_element(by=By.XPATH, value='//div[@class="c-PJLV c-PJLV-iiNveLf-css"]').text
-# # dict_properties['Bedrooms'].append(bedrooms)
-# # div_tag = driver.find_element(by=By.XPATH, value='//div[@data-testid="truncated_text_container"]')
-# # span_tag = div_tag.find_element(by=By.XPATH, value='.//span')
-# # description = span_tag.text
-# # dict_properties['Description'] = description
+# Note:
+# geckodriver is located at : /usr/local/bin
 
+# Questions:
+# 1) How to run locally Colab files
 
-# # crawler == creating a list of links
-
-
-
-# # # running a driver in headless mode
-
-# # from selenium import webdriver
-# # from selenium.webdriver.firefox.options import Options
-
-# # options = Options()
-# # options.headless = True
-# # driver = webdriver.Firefox(options=options, executable_path=r'C:\Utility\BrowserDrivers\geckodriver.exe')
-# # driver.get("http://google.com/")
-# # print ("Headless Firefox Initialized")
-# # driver.quit()
-
-# # Note:
-# # geckodriver is located at : /usr/local/bin
-
-# # Questions:
-# # 1) How to run locally Colab files
-
-# # change encoding, write html code to a 'result.html' file, close
-# # page = driver.page_source.encode('utf-8')
-# # file_ = open('result.html', 'wb')
-# # file_.write(page)
-# # file_.close()
-# # driver.close()
+# change encoding, write html code to a 'result.html' file, close
+# page = driver.page_source.encode('utf-8')
+# file_ = open('result.html', 'wb')
+# file_.write(page)
+# file_.close()
+# driver.close()
