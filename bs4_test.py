e971be6258e28a5c3722fd3ab27b159cc46cac1b
Piotr Z. Jelonek <piotr.z.jelonek@gmail.com>
2022-11-29 20:53:22 +0000

    Added OOP example for beautiful soup.

--- bs4_test.py
+++ bs4_test.py
@@ -59,4 +59,51 @@ for i in range(min(5,len(python_row))):
     print(python_row[i])
 print("")
 
+######################################################################
 
+# OOP: Example fo a class with private methods
+
+from bs4 import BeautifulSoup
+from typing import Union
+from typing import List
+import requests
+import re
+
+class AnimalReporter:
+    def __init__(self, animal: str):
+        self.animal = animal
+    
+    def _get_request(self) -> Union[bytes, str]:
+        ROOT = 'https://en.wikipedia.org/wiki/'
+        r = requests.get(ROOT + self.animal)
+        return r.text
+
+    def _get_soup(self, html: Union[bytes, str]) -> BeautifulSoup:
+        soup = BeautifulSoup(html, 'html.parser')
+        return soup
+        
+    def get_class(self) -> str:
+        html = self._get_request()
+        soup = self._get_soup(html)
+        class_row = soup.find('td', text = re.compile('Class:'))
+        animal_class = class_row.find_next_sibling().text.strip()
+        return animal_class
+    
+    def get_taxonomy(self) -> List:
+        html = self._get_request()
+        soup = self._get_soup(html)
+        syn_text = soup.find('a', text = re.compile('Synonyms'))
+        if syn_text:
+            syn_header = syn_text.find_parent('tr')
+            syn_table = syn_header.find_next_sibling()
+            contents = syn_table.find_all('i')
+            if contents:
+                contents = [x.text for x in contents]
+                return contents
+        else:
+            return []
+
+ar = AnimalReporter('koala')
+animal_class = ar.get_class()
+
+print(f"animal class: {animal_class}")
